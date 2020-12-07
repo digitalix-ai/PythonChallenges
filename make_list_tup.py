75b85f383ec33ab4d3d280f554350b89d8b581fb
digitalix-ai <lonely7hinker@gmail.com>
2020-12-07 04:52:31 +0000

    adding files

---
+++ make_list_tup.py
@@ -0,0 +1,4 @@
+seq = input('write a sequence of numbers: ')
+
+print(seq.split(','))
+print(tuple(seq.split(',')))
---
+++ square_dict.py
@@ -0,0 +1,8 @@
+n = int(input('write a number: '))
+
+result = {}
+
+for i in range(1, n + 1):
+	result[i] = i*i
+	
+print(result)
