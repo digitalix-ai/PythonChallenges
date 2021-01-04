37675ec31fa510982394e7e5ff876de2d6554d02
digitalix-ai <lonely7hinker@gmail.com>
2021-01-04 19:21:31 +0000

    adding new file

---
+++ sqroot.py
@@ -0,0 +1,14 @@
+from math import sqrt 
+
+C,H = 50,30
+
+def calc(D):
+    return sqrt((2*C*D)/H)
+
+D = [int(i) for i in input().split(',')]
+D = [int(i) for i in D]   
+D = [calc(i) for i in D]  
+D = [round(i) for i in D] 
+D = [str(i) for i in D]   
+
+print(",".join(D))
